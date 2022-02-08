81ec2e06a7f7a852866a3ec3194e8bab7f4373aa
EAAgafonov <ea.agafonov@yandex.ru>
2022-02-08 03:23:49 +0300

    initial commit

--- GeocoderProxyService/src/test/java/geocoderproxyservice/service/TracesDataServiceTest.java
+++ GeocoderProxyService/src/test/java/geocoderproxyservice/service/TracesDataServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
 
 import static org.mockito.Mockito.verify;
 
