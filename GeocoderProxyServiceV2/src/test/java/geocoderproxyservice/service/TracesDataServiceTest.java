071836e768cd42d8e7f903bac0133e70766e87db
EAAgafonov <ea.agafonov@yandex.ru>
2022-02-08 03:13:19 +0300

    initial commit

---
+++ GeocoderProxyServiceV2/src/test/java/geocoderproxyservice/service/TracesDataServiceTest.java
@@ -0,0 +1,46 @@
+package geocoderproxyservice.service;
+
+import com.geocoderproxyservice.data.TracesData;
+import com.geocoderproxyservice.repository.TracesDataRepository;
+import com.geocoderproxyservice.service.TracesDataService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TracesDataServiceTest {
+    @Mock
+    private TracesDataRepository tracesDataRepository;
+
+    private TracesDataService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new TracesDataService(tracesDataRepository);
+    }
+
+    @Test
+    void getTraces() {
+        underTest.getTraces();
+        verify(tracesDataRepository).findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    @Test
+    void saveTrace() {
+        TracesData test123 = new TracesData(
+                null,
+                "2022-02-03 20:18:06",
+                "POST",
+                "120ms",
+                200,
+                "/data/dt/"
+        );
+        underTest.saveTrace(test123);
+        verify(tracesDataRepository).save(test123);
+    }
+}
